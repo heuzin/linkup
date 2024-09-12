9291abc262091663357676c09c350072be6d4ea5
Matheus Silva <msilvaqs@outlook.com>
2024-09-12 14:29:38 -0400

    Creating authentication

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookierParser from 'cookie-parser';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://127.0.0.1:5173',
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  });
+  app.use(cookierParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((acc, error) => {
+          acc[error.property] = Object.values(error.constraints).join(', ');
+          return acc;
+        }, {});
+
+        throw new BadRequestException(formattedErrors);
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
