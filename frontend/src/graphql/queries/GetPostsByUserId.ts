87760a916b903d584c8d8ecce1dd25b2b643dfc2
Matheus Silva <msilvaqs@outlook.com>
2024-09-18 13:01:56 -0400

    Adding post page

---
+++ frontend/src/graphql/queries/GetPostsByUserId.ts
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+export const GET_POSTS_BY_USER_ID = gql`
+  query getPostsByUserId($userId: Float!) {
+    getPostsByUserId(userId: $userId) {
+      id
+      text
+      video
+      user {
+        fullname
+        email
+        id
+      }
+    }
+  }
+`;
