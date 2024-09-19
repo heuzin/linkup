56655331b0a66dbf221311044d1b7358194413e5
Matheus Silva <msilvaqs@outlook.com>
2024-09-18 20:46:37 -0400

    Updating profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import AuthModal from "./components/AuthModal";
+import EditProfileMoedal from "./components/EditProfileMoedal";
 import { useGeneralStore } from "./store/generalStore";
 
 function App() {
   const isLoginOpen = useGeneralStore((state) => state.isLoginOpen);
-  return <div>{isLoginOpen && <AuthModal />}</div>;
+  const isEditProfileOpen = useGeneralStore((state) => state.isEditProfileOpen);
+  return (
+    <div>
+      {isLoginOpen && <AuthModal />}{" "}
+      {isEditProfileOpen && <EditProfileMoedal />}
+    </div>
+  );
 }
 
 export default App;
